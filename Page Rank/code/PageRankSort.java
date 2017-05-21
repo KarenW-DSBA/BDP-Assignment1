3a3981bf5bc202eb5529b45a4d4f8f014864dcb5
KarenW-DSBA <karen.woo@essec.edu>
2017-05-21 14:23:27 -0700

    Update PageRankSort.java

--- Page Rank/code/PageRankSort.java
+++ Page Rank/code/PageRankSort.java
@@ -17,7 +17,7 @@ public class PageRankSort extends Mapper<LongWritable, Text,DoubleWritable, Text
         int index1 = value.find("\t");
         int index2 = value.find("\t", index1 + 1);
         
-        // extract tokens from the current line
+        // Extract tokens from the current line
         String vertex = Text.decode(value.getBytes(), 0, index1);
         float rank = Float.parseFloat(Text.decode(value.getBytes(),index1 + 1,index2 - (index1 + 1)));
         
